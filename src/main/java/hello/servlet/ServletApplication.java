bb5fa0576c004545d82994154e34ce830c62f74a
westkitekim <westkite418@gmail.com>
2022-06-30 09:31:46 +0900

    servlet

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동등록 : 스프링이 자동으로 하위패키지까지 서블릿 찾아 자동적으로 실행가능하게 만듦
 @SpringBootApplication
 public class ServletApplication {
 
